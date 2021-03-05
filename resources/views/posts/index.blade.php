0e60262d5a9ede5139baff10d9076c600ac052d7
edizzera <moraisedi.dev@gmail.com>
2021-03-05 15:45:50 -0300

    delete post by auth user

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,9 +30,20 @@
                         <a href="" class="font-bold">{{ $post->user->username }}</a> <span class="text-gray-600
                         text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         
-                        <p class="mb-2">{{ $post->body }}</p>
+                        <p class="mb-2">{{ $post->body }}</p> 
+                        
+                        @can('delete', $post)                            
+                        <form action="{{ route('posts.destroy',$post)}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="text-green-500"><i class="fa fa-trash" aria-hidden="true"></i></button>
+                        </form>
+                        @endcan
+                            
+                        
 
                         <div class="flex items-center">
+                            @auth
                             @if (!$post->likedBy(auth()->user()))
                                 <form action="{{route('posts.likes', $post)}}" method="post" class="mr-1">
                                     @csrf
@@ -45,6 +56,7 @@
                                     <button type="submit" class="text-green-500"><i class="fa fa-thumbs-down" aria-hidden="true"></i></button>
                                 </form>
                             @endif
+                            @endauth
                             <span class="ml-2">{{ $post->likes->count() }} {{ Str::plural('like',
                             $post->likes->count()) }}</span>
                         </div>
